9e7697124d5160f56e63a519b257905e187adab4
Caleb McCombs <s209019@students.aie.edu>
2021-08-18 16:53:35 -0700

    initial commit, added fixed timestop excercises

---
+++ FixedTimestepExercises/Assets/Scripts/FrameratThrottler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameratThrottler : MonoBehaviour
+{
+    public int targetFramerate = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = targetFramerate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Application.targetFrameRate = targetFramerate;
+    }
+}
---
+++ FixedTimestepExercises/Assets/Scripts/SimpleRigidbody.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRigidbody : MonoBehaviour
+{
+    public Vector3 velocity;
+
+    public bool useGravity;
+
+    public enum UpdateMode
+    {
+        None,
+        Update,
+        FixedUpdate
+    }
+    public UpdateMode updateMode;
+
+    private void Update()
+    {
+        // early exit if we aren't using this UpdateMode
+        if (updateMode != UpdateMode.Update) { return; }
+
+        UpdateVelocity();
+        UpdatePosition();
+    }
+
+    private void FixedUpdate()
+    {
+        // early exit if we aren't using this UpdateMode
+        if (updateMode != UpdateMode.FixedUpdate) { return; }
+
+        UpdateVelocity();
+        UpdatePosition();
+    }
+
+    private void UpdateVelocity()
+    {
+        // integrate gravitational forces into velocity
+        if (useGravity)
+        {
+            // apply the acceleration of gravity
+            velocity += Physics.gravity * Time.deltaTime;
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        // integrate velocity into position
+        transform.position += velocity * Time.deltaTime;
+    }
+}
