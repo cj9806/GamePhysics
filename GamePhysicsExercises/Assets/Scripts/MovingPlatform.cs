31686f686bf7e034bc4792981341a09df16e4506
Caleb McCombs <s209019@students.aie.edu>
2021-08-19 12:56:59 -0700

    pushed rigidbody exercises

---
+++ GamePhysicsExercises/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] Vector3[] waypoints;
+    private int waypointIndex;
+    public float moveSpeed;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypointIndex = 0;
+        rb = this.GetComponent<Rigidbody>();
+        moveSpeed *= .1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 waypoint = Vector3.MoveTowards(transform.position, waypoints[waypointIndex], moveSpeed);
+        rb.MovePosition(waypoint);
+        if (this.transform.position == waypoints[waypointIndex])
+        {
+            if (waypointIndex == waypoints.Length-1) waypointIndex = 0;
+            else waypointIndex++;
+        }
+    }
+}
---
+++ GamePhysicsExercises/Assets/Scripts/SlimeMotor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*need to hop towards particular location
+* controll the jump power vericly
+* control jump power horizontily
+* jump interval
+* should be implemented using Rigidbody.AddForce
+*/
+public class SlimeMotor : MonoBehaviour
+{
+    public float verticleJumpPower;
+    public float horizontalJumpPower;
+    public float jumpInterval;
+    public Vector3 travelPoint;
+    //goal is 4,.75,0
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    private bool closeToGoal;
+    float elapsed;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        closeToGoal = false;
+        elapsed = 0f;
+        verticleJumpPower *= 100;
+        horizontalJumpPower *= 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= jumpInterval)
+        {
+            elapsed = 0f;
+            if (!closeToGoal)
+            {
+                Jump();
+                if (this.transform.position.x >= 3)
+                    closeToGoal = true;
+            }
+        }
+
+    }
+    void Jump()
+    {
+        if (this.transform.position.y == .75)
+        {
+            rb.velocity = Vector3.zero;
+            rb.AddForce(horizontalJumpPower, verticleJumpPower, 0);
+        }
+    }
+}
