5a0afc8fb3488167cb711485dd970df744779ede
Caleb McCombs <s209019@students.aie.edu>
2021-08-19 17:53:40 -0700

    worked on slime game

--- GamePhysicsExercises/Assets/Scripts/SlimeMotor.cs
+++ GamePhysicsExercises/Assets/Scripts/SlimeMotor.cs
@@ -16,6 +16,7 @@ public class SlimeMotor : MonoBehaviour
     //goal is 4,.75,0
     // Start is called before the first frame update
     private Rigidbody rb;
+    private System.Random random;
     private bool closeToGoal;
     float elapsed;
     void Start()
@@ -24,21 +25,37 @@ public class SlimeMotor : MonoBehaviour
         closeToGoal = false;
         elapsed = 0f;
         verticleJumpPower *= 100;
-        horizontalJumpPower *= 100;
+        //horizontalJumpPower *= 100;
+        random = new System.Random();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //basic timer
         elapsed += Time.deltaTime;
+        //after given time
         if (elapsed >= jumpInterval)
         {
+            //reset timer
             elapsed = 0f;
+            //if not close to goal
             if (!closeToGoal)
             {
+                //jump up
                 Jump();
-                if (this.transform.position.x >= 3)
+                //move forward
+
+                //see if close to goal
+                if (travelPoint.x - transform.position.x <= 1 && travelPoint.z - transform.position.z <= 1)
+                {
                     closeToGoal = true;
+                }
+            }
+            else
+            {
+                travelPoint = createNewWaypoint();
+                closeToGoal = false;
             }
         }
 
@@ -47,8 +64,15 @@ public class SlimeMotor : MonoBehaviour
     {
         if (this.transform.position.y == .75)
         {
-            rb.velocity = Vector3.zero;
-            rb.AddForce(horizontalJumpPower, verticleJumpPower, 0);
+            rb.AddForce(0, verticleJumpPower, 0);
+            rb.AddForce((travelPoint - transform.position).normalized * horizontalJumpPower * Time.deltaTime);
+            //jump forward
         }
+    }
+    Vector3 createNewWaypoint()
+    {
+        int randX = random.Next(-23, 23);
+        int randZ = random.Next(-23, 23);
+        return new Vector3(randX, .75f, randZ);
     }
 }
