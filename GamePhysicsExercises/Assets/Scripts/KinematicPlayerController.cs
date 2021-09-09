abb0ae6032412666267e30160b51abff58b60b2d
Caleb McCombs <s209019@students.aie.edu>
2021-09-08 17:52:32 -0700

    worked on plyaer controller

--- GamePhysicsExercises/Assets/Scripts/KinematicPlayerController.cs
+++ GamePhysicsExercises/Assets/Scripts/KinematicPlayerController.cs
@@ -20,30 +20,38 @@ public class KinematicPlayerController : MonoBehaviour
     //[SerializeField] 
 
     public float skinWidth;
-    Collider col;
+    private Vector3 skin;
+    BoxCollider col;
     
     Vector3 expp;
 
     public PlayerInput input;
+    bool pickingUp;
+
+    public float playerReach;
+    FixedJoint joint;
+    [SerializeField] LayerMask grabableLayers;
     // Start is called before the first frame update
     void Start()
     {
-        col = this.GetComponentInChildren<Collider>();
-        col.transform.localScale = transform.localScale + (Vector3.one * skinWidth);
+        col = this.GetComponentInChildren<BoxCollider>();
+        skin = col.size + (Vector3.one * skinWidth);
+        //col
         Cursor.lockState = CursorLockMode.Locked;
+        joint = GetComponent<FixedJoint>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(lookX + " " + lookY);
         expp = transform.position;
         //movement handling events
         {
+            //looking
             Vector2 LookInput = input.currentActionMap["Look"].ReadValue<Vector2>();
             lookX = LookInput.x;
             lookY = LookInput.y;
-
+            //moving
             Vector2 moveInput = input.currentActionMap["Move"].ReadValue<Vector2>();
             tempX = moveInput.x;
             tempZ = moveInput.y;
@@ -55,8 +63,18 @@ public class KinematicPlayerController : MonoBehaviour
             {
                 expp += transform.forward * tempZ * moveSpeed;
             }
-            
-            
+            //pick up animations
+            //if right click and there is no held object pick up
+            //elseif right click and there is a held object do nothing
+            //elseif no right click
+            if (pickingUp && this.gameObject.GetComponent <FixedJoint>()== null) 
+            {
+                PickUp();
+            }
+            else if(!pickingUp && this.gameObject.GetComponent<FixedJoint>() != null)
+            {
+                Drop();
+            }
             //jump handling
             if (useGravity)
             {
@@ -72,7 +90,7 @@ public class KinematicPlayerController : MonoBehaviour
         }
         //colision handling
         
-        Collider[] overlaps = Physics.OverlapBox(transform.position,transform.localScale/2);
+        Collider[] overlaps = Physics.OverlapBox(expp, skin, col.transform.rotation);
         foreach (var overlap in overlaps)
         {
             if (overlap == col) continue;
@@ -92,10 +110,26 @@ public class KinematicPlayerController : MonoBehaviour
             //draw a ray for testing purposes
             Debug.DrawRay(this.transform.position, cldrDir * cldrDist, Color.red);
             if (overlap.name == "Floor")
-                expp += cldrDir * -gravityStrength;
+                expp += cldrDir * cldrDist;
             else expp += cldrDir * moveSpeed;
 
         }
         transform.position = expp;
+    }
+    void OnPickUp(InputValue value)
+    {
+        pickingUp = value.isPressed;
+    }
+
+    void PickUp()
+    {
+        var ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Physics.Raycast(ray, out RaycastHit hit, playerReach, grabableLayers);
+        GameObject slimeColider = hit.collider.gameObject;
+        joint.connectedBody = slimeColider.GetComponent<Rigidbody>();
+    }
+    void Drop()
+    {
+        Destroy(this.GetComponent<FixedJoint>());
     }
 }
